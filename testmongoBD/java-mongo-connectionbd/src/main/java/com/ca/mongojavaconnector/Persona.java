158f92efdaba52ee7988404ef04cb322e5178b9e
SHW378 <cesarbrmn@hotmail.com>
2025-04-10 00:26:31 -0600

    setters, getters

--- testmongoBD/java-mongo-connectionbd/src/main/java/com/ca/mongojavaconnector/Persona.java
+++ testmongoBD/java-mongo-connectionbd/src/main/java/com/ca/mongojavaconnector/Persona.java
@@ -13,6 +13,38 @@ public class Persona {
         this.email = email;
     }
 
+    public String getnombre() {
+        return nombre;
+    }
+
+    public void setnombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getedad() {
+        return edad;
+    }
+
+    public void setedad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getciudad(){
+        return ciudad;
+    }
+
+    public void setciudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getemail() {
+        return email;
+    }
+
+    public void setemail(String email) {
+        this.email = email;
+    }
+
     @Override
     public String toString() {
         return "Persona{" +
