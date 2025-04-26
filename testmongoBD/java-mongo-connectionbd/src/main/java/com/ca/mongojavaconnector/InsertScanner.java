3365b4281b29464e6376b8b8c9d72cf51d0785bf
SHW378 <cesarbrmn@hotmail.com>
2025-04-26 01:07:19 -0600

    esobrad

--- testmongoBD/java-mongo-connectionbd/src/main/java/com/ca/mongojavaconnector/InsertScanner.java
+++ testmongoBD/java-mongo-connectionbd/src/main/java/com/ca/mongojavaconnector/InsertScanner.java
@@ -16,40 +16,24 @@ public class InsertScanner {
         try (MongoClient mongoClient = MongoClients.create(uri)) {
             System.out.println();
             System.out.println("Ingrese la base de datos a usar (si no existe se creara)");
-            MongoDatabase database = mongoClient.getDatabase(sc.nextLine());
-            System.out.println("Ingrese la colección a usar (si no existe se creara)");
-            MongoCollection<Document> collection = database.getCollection(sc.nextLine());
-
-            Document doc = new Document();
-
-            String continuar;
-            for (;;) {
-                System.out.println("Ingrese el nombre del atributo: ");
-                String atributo = sc.nextLine();
-                System.out.println("Ingrese el valor del atributo: ");
-                String input = sc.nextLine();
-
-                Object valor;
-                try {
-                    valor = Integer.parseInt(input);
-                } catch (NumberFormatException e) {
-                    valor = input;
-                }
-                doc.append(atributo, valor);
-
-                for (;;) {
-                    System.out.println("¿Desea agregar otro atributo? (s/n)");
-                    continuar = sc.nextLine().toLowerCase();
-                    if (continuar.equals("s")) {
-                        break;
-                    } else if (continuar.equals("n")) {
-                        collection.insertOne(doc);
-                        System.out.println("Documento Insertado con éxito");
-                        return;
-                    }
-                    System.out.println("Caracter inválido. Por favor, ingrese 's' para continuar o 'n' para salir.");
-                }
+            String DBname = sc.nextLine();
+            if (DBname.isEmpty()) {
+                System.out.println("El nombre de la base de datos no puede estar vacío.");
+                return;
             }
+            MongoDatabase database = mongoClient.getDatabase(DBname);
+
+            System.out.println("Ingrese la colección a usar (si no existe se creara)");
+            String collectionName = sc.nextLine();
+            if (collectionName.isEmpty()) {
+                System.out.println("El nombre de la colección no puede estar vacío.");
+                return;
+            }
+            MongoCollection<Document> collection = database.getCollection(collectionName);
+
+            crearDocumento(sc, collection);
+
+            System.out.println("Documento insertado con exito");
         } catch (MongoTimeoutException e) {
             System.err.println("Error en la conexión a MongoDB.");
         } catch (Exception e) {
@@ -57,5 +41,50 @@ public class InsertScanner {
         } finally {
             sc.close();
         }
+    }
+
+    private static Document crearDocumento(Scanner sc, MongoCollection<Document> collection) {
+        Document doc = new Document();
+        boolean continuarAgregando = true;
+
+        while (continuarAgregando) {
+            System.out.println("Ingrese el nombre del atributo");
+            String atributo = sc.nextLine();
+            if (atributo.isEmpty()) {
+                System.out.println("El nombre del atributo no puede estar vacío.");
+                continue;
+            }
+
+            System.out.println("Ingrese el valor del atributo");
+            String input = sc.nextLine();
+            Object valor;
+            try {
+                valor = Integer.parseInt(input);
+            } catch (NumberFormatException e) {
+                valor = input;
+            }
+
+            if (valor.toString().isEmpty()) {
+                System.out.println("El valor del atributo no puede estar vacío.");
+                continue;
+            }
+
+            doc.append(atributo, valor);
+
+            while (true) {
+                System.out.println("¿Desea agregar otro atributo? (s/n)");
+                String continuar = sc.nextLine().toLowerCase();
+                if (continuar.equals("s")) {
+                    break;
+                } else if (continuar.equals("n")) {
+                    continuarAgregando = false;
+                    break;
+                } else {
+                    System.out.println("Opción no válida. Por favor, ingrese 's' o 'n'.");
+                }
+            }
+        }
+        collection.insertOne(doc);
+        return doc;
     }
 }
